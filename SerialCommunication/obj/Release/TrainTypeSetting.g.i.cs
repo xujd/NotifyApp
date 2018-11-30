2fb4b6ca3093dc71c7c9d2df10719330920e93c8
Andy Hsu(许俊东) <xu.jd@inspur.com>
2018-11-30 15:31:15 +0800

    modify

--- SerialCommunication/obj/Release/TrainTypeSetting.g.i.cs
+++ SerialCommunication/obj/Release/TrainTypeSetting.g.i.cs
@@ -31,6 +31,7 @@ using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
+using Xceed.Wpf.Toolkit;
 
 
 namespace SerialCommunication {
